6ed5f0e978b08b1432d5fb2c45f8c8cead62b26b
Yura Sidorov <odessa202@gmail.com>
2021-02-13 23:00:38 +0200

    Added Timer

---
+++ Clicker/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    Coroutine _coroutine;
+
+    public TimeSpan Interval { get; set; }
+
+    public event Action Tick;
+
+    public void Begin() => _coroutine = StartCoroutine(Iteration());
+
+    IEnumerator Iteration()
+    {
+        while (true)
+        {
+            Tick?.Invoke();
+            yield return new WaitForSecondsRealtime((float)Interval.TotalSeconds);
+        }
+    }
+
+    public void End() => StopCoroutine(_coroutine);
+}
